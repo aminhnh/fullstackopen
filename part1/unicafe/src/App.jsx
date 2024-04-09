5de9f1efe754d2aaf37522b6d56789e82b23f17b
aminhnh <109867158+aminhnh@users.noreply.github.com>
2024-04-09 23:31:36 +0700

    Exercise 1.8: unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,6 +8,35 @@ const Button = ({onClick, text}) => {
   )
 }
 
+const Statistics = ({good, neutral, bad}) => {
+  const getTotal = () => good + neutral + bad
+  const getAverage = () => {
+    if (getTotal() === 0) {
+      return 0
+    }
+    return (good - bad)/getTotal()
+  }
+  const getPositive = () => {
+    return getTotal() === 0 ? (
+      0
+    ) : (
+      good/getTotal()
+    )
+  }
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p>Good : {good}</p>
+      <p>Neutral : {neutral}</p>
+      <p>Bad : {bad}</p>
+      <br />
+      <p>All : {getTotal()}</p>
+      <p>Average : {getAverage()}</p>
+      <p>Positive : {getPositive()}%</p>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -17,37 +46,13 @@ const App = () => {
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
 
-  const getTotal = () => good + neutral + bad
-  
-  const getAverage = () => {
-    if (getTotal() === 0) {
-      return 0
-    }
-    return (good - bad)/getTotal()
-  }
-
-  const getPositive = () => {
-    return getTotal() === 0 ? (
-      0
-    ) : (
-      good/getTotal()
-    )
-  }
-
   return (
     <div>
       <h2>Give feedback</h2>
       <Button onClick={handleGood} text="good"/> &nbsp; 
       <Button onClick={handleNeutral} text="neutral"/> &nbsp; 
       <Button onClick={handleBad} text="bad"/>
-      <h2>Statistics</h2>
-      <p>Good : {good}</p>
-      <p>Neutral : {neutral}</p>
-      <p>Bad : {bad}</p>
-      <br />
-      <p>All : {getTotal()}</p>
-      <p>Average : {getAverage()}</p>
-      <p>Positive : {getPositive()}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
